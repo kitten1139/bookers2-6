0f77b422c4da89e483cac086bfd05b934d1e1cc3
kitten1139 <106716550+kitten1139@users.noreply.github.com>
2022-07-16 06:34:17 +0000

    [Finish]bookers2-5

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,6 @@ Rails.application.routes.draw do
   get :followers, on: :member
   get :follows, on: :member
 
-
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
