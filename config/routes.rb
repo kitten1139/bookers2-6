be14edcbbb4f591ae2751adc86e9cc2380e6114c
kitten1139 <106716550+kitten1139@users.noreply.github.com>
2022-07-16 05:12:15 +0000

    [Add]searchesコントローラ追加

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
     resource :relationships, only: [:create, :destroy]
   get :followers, on: :member
   get :follows, on: :member
+
+  get "search" => "searches#search"
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
